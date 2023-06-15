ec726abb406ac5aacf3cef3300e64d3345a85579
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-15 16:29:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
- * Main - A program that prints a multilingual puzzle
+ * main - A program that prints a line with the puts function
  * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
