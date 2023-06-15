3029799ce26875f13904ef1b7a3baadb029b6018
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-15 15:56:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * Main - A program that prints a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
