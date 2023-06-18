7173529cea8490b25e88105b4b178ccb65252c8f
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-18 15:07:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - A function that prints alphabet in lowercase and uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char k;
+
+	char n;
+
+	k = 'a';
+	n = 'A';
+	while
+		(k <= 'z') {
+			putchar(k);
+			k++;
+		}
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
