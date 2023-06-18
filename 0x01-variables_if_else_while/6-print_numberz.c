1416ae8a0658301157b5ba4013ffd04efd3779e3
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-18 15:46:34 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function that prints base 10 numbers without using char
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 48; k < 58; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
