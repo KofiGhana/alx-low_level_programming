a22bfb446a22299d9199141fefe22d06c3615fb6
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-18 15:41:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Function that prints digit numbers of base 10 with a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+		printf("%d", k);
+	printf("\n");
+	return (0);
+}
