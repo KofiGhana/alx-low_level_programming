0e2b43aa84e89fbfaf3ba63b3e1aec9f9ecf4e84
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-18 14:56:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A function that prints alphabet in lowercase with a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'a';
+	while
+		(k <= 'z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
