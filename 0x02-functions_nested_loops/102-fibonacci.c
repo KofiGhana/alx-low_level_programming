bd450fa4ac682f3755ee35aab18f60f4c4b76365
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-20 14:59:07 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,21 @@
 
 int main(void)
 {
-	int k;
-	unsigned long fibo1 = 0, fibo2 = 1, sum;
+        int k;
+        unsigned long fibo1 = 0, fibo2 = 1, sum;
 
-	for (k = 0; k < 50; k++)
-	{
-		sum = fibo1 + fibo2;
-		printf("%ld", sum);
+        for (k = 0; k < 50; k++)
+        {
+                sum = fibo1 + fibo2;
+                printf("%lu", sum);
 
-		fibo1 = fibo2;
-		fibo2 = sum;
+                fibo1 = fibo2;
+                fibo2 = sum;
 
-		if (k == 49)
-			printf("\n");
-		else
-			printf(",");
-	}
-	return (0);
+                if (k == 49)
+                        printf("\n");
+                else
+                        printf(",");
+        }
+        return (0);
 }
