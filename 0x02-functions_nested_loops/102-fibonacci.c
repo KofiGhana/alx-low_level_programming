5433bdb854d8fa2250ab47a70d2fed7f20144787
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-20 14:32:29 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Function that prints the first 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k;
+	unsigned long fibo1 = 0, fibo2 = 1, sum;
+
+	for (k = 0; k < 50; k++)
+	{
+		sum = fibo1 + fibo2;
+		printf("%lu", sum);
+
+		fibo1 = fibo2;
+		fibo2 = sum;
+
+		if (k == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
