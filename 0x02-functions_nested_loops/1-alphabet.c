56d820862905c384b8145628ad61c1753bf7440a
KofiGhana <abkarikari23@yahoo.co.uk>
2023-06-20 05:30:10 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Function that prints alphabets in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char kn;
+
+	for (kn = 'a'; kn <= 'z'; kn++)
+		_putchar(kn);
+
+	_putchar('\n');
+}
